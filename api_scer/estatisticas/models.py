5e39a724f06eabcef19a9668125c5aee2f0fbfd7
Davizao <davi.rguedes@outlook.com>
2025-01-21 19:43:53 -0300

    Aula 06 - Primeiro Model e Criação do Superuser

--- api_scer/estatisticas/models.py
+++ api_scer/estatisticas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+CLASSIFICACAO = (
+    ('category', 'category'),
+    ('food', 'food'),
+    ('eletronico', 'eletronico'),
+)
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200, null=True)
+    classificacao = models.CharField(max_length=200, choices=CLASSIFICACAO , null=True)
+    quantidade = models.PositiveIntegerField(null=True)
+    
+    def __str__(self):
+        return f'{self.nome}-{self.quantidade}'
+    
