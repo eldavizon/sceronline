1f57fbe3a11e040f98d0eb62baf4caf6a91f4a14
Davizao <davi.rguedes@outlook.com>
2025-01-22 15:09:32 -0300

    Aula 07

--- api_scer/estatisticas/admin.py
+++ api_scer/estatisticas/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Produto
+from django.contrib.auth.models import Group
 
 # Register your models here.
 
-admin.site.register(Produto)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('nome','classificacao', 'quantidade')
+    list_filter = ('classificacao',)
+    
+
+admin.site.register(Produto, ProductsAdmin)
+
+#admin.site.unregister(Group)
+
+admin.site.site_header = 'Sistema de Controle de Estoque de Reagentes'
