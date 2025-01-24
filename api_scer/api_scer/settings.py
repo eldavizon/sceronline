f83fa7a529ca713daed4a303ca7e38b960f69c28
Davizao <davi.rguedes@outlook.com>
2025-01-24 16:25:42 -0300

    aula 13 - criação do perfil e obtençao dos dados do model profile

--- api_scer/api_scer/settings.py
+++ api_scer/api_scer/settings.py
@@ -131,6 +131,10 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = (BASE_DIR/"assets/")
 
+MEDIA_ROOT = (BASE_DIR/'media')
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- api_scer/api_scer/urls.py
+++ api_scer/api_scer/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from user import views as user_view
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('estatisticas.urls')), #importando o arquivo de urls do app de estatisticas
     path('register/', user_view.register, name='user-register'),
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout')
-]
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
+    path('profile/', user_view.profile, name='user-profile'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
